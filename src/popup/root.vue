f5610404e4f3499abb8a7ebe4af29d5dedc9b06a
Niguai <niguai.lee@tenoz.tw>
2019-03-03 12:35:33 +0800

    Vue init

--- src/popup/root.vue
+++ src/popup/root.vue
@@ -1,19 +1,32 @@
-<template lang="pug">
-    div
-      el-button(type="primary" @click="tab") New tab
+<template lang="html">
+  <div ref="btn" class="el-button el-button--primary" @click="tab">
+    <span>New tab</span>
+  </div>
 </template>
 <script>
+  import cheerio from 'cheerio'
+
   export default {
+    props: ['display'],
     data: () => ({
+      status: 1
     }),
     computed: { },
+    watch: {
+      display () {
+
+      }
+    },
     created () {
       console.log('New tab')
     },
     mounted () { },
     methods: {
       tab () {
-        chrome.tabs.create({ url: 'pages/app.html' })
+        console.log('change')
+        console.log(this.$refs['btn'])
+        console.log(cheerio)
+        // chrome.tabs.create({ url: 'pages/app.html' })
       }
     }
   }
